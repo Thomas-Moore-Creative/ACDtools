2cb18022e549104fea09b12baea2fdcadc5c2be5
Thomas-Moore-Creative <thomas.moore@csiro.au>
2024-09-30 16:00:32 +1000

    add test function

--- ACDtools/util.py
+++ ACDtools/util.py
@@ -3,3 +3,7 @@ util.py
 
 This module contains a collection of utility functions for Australian Climate Data (ACD) tools at NCI.
 """
+
+def test_function():
+    print("The ACDtools package is installed and working correctly.")
+    return True
