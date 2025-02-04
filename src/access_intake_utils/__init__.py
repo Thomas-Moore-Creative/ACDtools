73e2c7faf04ab8966d3045c90295074177a71a09
Charles Turner <charles.turner@anu.edu.au>
2025-02-04 15:34:46 +0800

    Set up versioneer

--- src/access_intake_utils/__init__.py
+++ src/access_intake_utils/__init__.py
@@ -3,3 +3,5 @@
 # 1. Import necessary submodules or functions to expose them at the package level
 #from .module1 import main_function
 #from .module2 import helper_function
+from . import _version
+__version__ = _version.get_versions()['version']
